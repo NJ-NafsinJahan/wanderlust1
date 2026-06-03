6746874e3f4801e426cc7f14ef78c8c1072d59ca
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-06-03 09:48:25 +0600

    POST API

--- src/app/add-destination/page.jsx
+++ src/app/add-destination/page.jsx
@@ -4,13 +4,32 @@ import { Card, FieldError, Input, TextArea, TextField } from "@heroui/react";
 import { Label, Select, ListBox, Button } from "@heroui/react";
 
 const AddDestinationPage = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+
+    console.log(destination, "formData working or not: ");
+
+    const res = await fetch("http://localhost:5000/destination", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(destination),
+    });
+    const data = await res.json();
+
+    console.log(data, "Data collect from backend api");
+  };
+
   return (
     <div className="p-6 max-w-4xl mx-auto items-center">
       <h1 className="font-bold text-2xl text-cyan-500 mb-2">Add Destination</h1>
 
       {/* form */}
       <Card>
-        <form className="p-10 space-y-8">
+        <form onSubmit={onSubmit} className="p-10 space-y-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {/* Destination Name */}
             <div className="md:col-span-2">
