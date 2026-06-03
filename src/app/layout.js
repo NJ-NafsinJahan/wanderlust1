ef9ddfc3f67ccd251259bd28302399850edc47bf
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-06-03 12:18:26 +0600

    destination card

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const josefinSans = Josefin_Sans({
   subsets: ["latin"],
 });
 
@@ -20,10 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${josefinSans.className} h-full antialiased`}>
       <body>
         <Navbar></Navbar>
         <main>{children}</main>
---
+++ src/components/DestinationCard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+import { FiArrowUpRight, FiMapPin } from "react-icons/fi";
+import { IoCalendarOutline } from "react-icons/io5";
+import { Button } from "@heroui/react";
+
+const DestinationCard = ({ destination }) => {
+  const { destinationName, imageUrl, country, price, duration } = destination;
+  return (
+    <div className="border m-4 p-3">
+      {/* <h1>Destination Card</h1> */}
+
+      <Image src={imageUrl} alt={destinationName} width={400} height={400} />
+
+      <div className="mt-5">
+        <p className="flex items-center gap-3">
+          {" "}
+          <FiMapPin />
+          {country}
+        </p>
+
+        <div className="mt-2">
+          <h1 className="font-bold text-xl">{destinationName}</h1>
+        </div>
+
+        <div className="flex items-center gap-7 mt-3">
+          <p className="flex items-center gap-3">
+            <IoCalendarOutline />
+            {duration} Days
+          </p>
+          <p className="flex items-center">
+            <span className="font-bold text-[16px]">${price}</span>/person
+          </p>
+        </div>
+      </div>
+      <Button
+        fullWidth
+        className="mt-3 bg-cyan-400 text-black font-semibold text-[16px]"
+      >
+        {" "}
+        Book Now <FiArrowUpRight />
+      </Button>
+    </div>
+  );
+};
+
+export default DestinationCard;
